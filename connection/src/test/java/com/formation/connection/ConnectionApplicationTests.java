0d7986d27a64c00ea4a1797eb1620d089b5740f0
BabaSouley <Babatambadou@gmail.com>
2020-03-04 15:15:37 +0100

    [ORIGIN][MASTER][ADD] add folder connection service

---
+++ connection/src/test/java/com/formation/connection/ConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
